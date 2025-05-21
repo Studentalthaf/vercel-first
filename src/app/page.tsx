d17ce7b03b452d07f15cd20dd58b20869b469716
Studentalthaf <09010621013@student.uinsby.ac.id>
2025-05-21 21:26:36 +0700

    manmabah rr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            ALTHAF JAUHAR
+            ALTHAF JAUHAR RR
           </li>
         </ol>
 
